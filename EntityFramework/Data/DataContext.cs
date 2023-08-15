27da8c7583a9b15ae99d9c381ff6d7d291bdc272
Ant <hark777@hotmail.com>
2023-08-15 14:34:02 +1000

    Add data context

---
+++ EntityFramework/Data/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework.Data;
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+
