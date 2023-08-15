a7e11abb55d8eed06d9e663cf2013f05addde39f
Ant <hark777@hotmail.com>
2023-08-15 16:42:47 +1000

    Add one to many

--- EntityFramework/Data/DataContext.cs
+++ EntityFramework/Data/DataContext.cs
@@ -7,5 +7,6 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     public DbSet<Character> Characters { get; set; }
     public DbSet<Backpack> Backpacks { get; set; }
+    public DbSet<Weapon> Weapons { get; set; }
 }
 
