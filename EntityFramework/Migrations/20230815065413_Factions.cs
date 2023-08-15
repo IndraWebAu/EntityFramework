e0cde2ad1abd6f37c98237ab87032dbcf54600b0
Ant <hark777@hotmail.com>
2023-08-15 16:56:23 +1000

    add many to many relationship

---
+++ EntityFramework/Migrations/20230815065413_Factions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Factions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Factions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterFaction",
+                columns: table => new
+                {
+                    CharactersId = table.Column<int>(type: "int", nullable: false),
+                    MyPropertyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterFaction", x => new { x.CharactersId, x.MyPropertyId });
+                    table.ForeignKey(
+                        name: "FK_CharacterFaction_Characters_CharactersId",
+                        column: x => x.CharactersId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CharacterFaction_Factions_MyPropertyId",
+                        column: x => x.MyPropertyId,
+                        principalTable: "Factions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterFaction_MyPropertyId",
+                table: "CharacterFaction",
+                column: "MyPropertyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterFaction");
+
+            migrationBuilder.DropTable(
+                name: "Factions");
+        }
+    }
+}
