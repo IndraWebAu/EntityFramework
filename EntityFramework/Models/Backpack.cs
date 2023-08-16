177b5ab216617134ca7aeecb6dcfb27bb4a6f832
Ant <hark777@hotmail.com>
2023-08-16 13:07:44 +1000

    change backpack col

--- EntityFramework/Models/Backpack.cs
+++ EntityFramework/Models/Backpack.cs
@@ -3,7 +3,7 @@
     public class Backpack
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Description { get; set; }
         public int CharacterId { get; set; }
         public Character Character { get; set; }
     }
